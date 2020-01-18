b2f27e5fb7ffdb74df2f844a44a0dff704f935b3
RC Bellergy <rc@bellergy.com>
2020-01-18 16:49:18 +0800

    changed to monitor zerotier network speed

--- home/bellergy/tools/network-speed.py
+++ home/bellergy/tools/network-speed.py
@@ -9,20 +9,23 @@ import time
 import pymavlink.mavutil as mavutil
 
 
-UDP = "192.168.192.103:14550" # The IP and port of QGroundcontrol. It can't be a broadcast IP.
+NETWORK = "zt2lrwgvd2" # The Zerotier network interface
+UDP = "192.168.192.101:14550" # The IP and port of QGroundcontrol. (Can't it be a broadcast IP? I tried 192.168.192.255 but not work)
 SOURCE_SYSTEM_ID = 99 # Me, the sender
 TARGET_SYSTEM_ID = 255 # QGroundcontrol
 
 seq = 0
-interval = 1 # send the result every 5 sec.
+interval = 2 # send the result every 2 sec.
 download = None
 upload = None
 
 mav = mavutil.mavlink_connection('udpout:' + UDP, source_system=SOURCE_SYSTEM_ID, dialect="common")
 print("Mavlink2.0:", mav.mavlink20())
+sh_command = "awk '/%s/ {print $2, $10}' /proc/net/dev" % NETWORK
+print("Monitoring Network: %s" % NETWORK)
 
 while True:
-    stream = os.popen("awk '/wlan0/ {print $2, $10}' /proc/net/dev")
+    stream = os.popen(sh_command)
     output = stream.read()
     data = output.split()
     now_download = int(data[0])
@@ -33,12 +36,14 @@ while True:
         output_u = float(now_upload - upload)/1024/interval
         print("Rx: %f | Tx: %f" % (output_d, output_u))
 
-        # Send the result to mavlink
+        # Send the result to mavlink (Under testing)
+        # Changed to mavlink v2 and dialect to "common", but still command not found. Why?
         # Using message https://mavlink.io/en/messages/common.html#ONBOARD_COMPUTER_STATUS
+
         seq = seq +1
-        # msg = mav.mav.ping_send(int(time.time() * 1000), seq, TARGET_SYSTEM_ID, 1)
+        msg = mav.mav.ping_send(int(time.time() * 1000), seq, TARGET_SYSTEM_ID, 1) # PING and other Mavlink v1 commands work.
         # msg = mav.mav.param_ext_value_send("Testing","Hello",3,4,5)
-        msg = mav.mav.onboard_computer_status_send(int(time.time() * 1000), int(time.time() * 1000), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+        # msg = mav.mav.onboard_computer_status_send(int(time.time() * 1000), int(time.time() * 1000), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
         
 
     download = now_download
