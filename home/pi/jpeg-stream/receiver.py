4db880322065ea36be2031430671482daf3be7c3
RC Bellergy <rc@bellergy.com>
2020-02-16 15:51:27 +0800

    Keep the receiver alive

--- home/pi/jpeg-stream/receiver.py
+++ home/pi/jpeg-stream/receiver.py
@@ -10,6 +10,8 @@ import cv2
 TCP_IP = "0.0.0.0"
 TCP_PORT = 5800
 
+conn = None
+
 # Method to grab the size of the image array and verify that it is being decoded correctly
 def recvall(sock, count):
     buf = b''
@@ -20,57 +22,77 @@ def recvall(sock, count):
         count -= len(newbuf)
     return buf
 
-# Create a new socket with standard SOCK STREAM settings
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def attemptConnection():
 
-# Set socket to be able to reconnect immediately and ignore timeouts
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    global conn
 
-# Bind socket to IP and port supplied from constructor
-s.bind((TCP_IP, TCP_PORT))
+    # Create a new socket with standard SOCK STREAM settings
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Listen for exactly 1 host
-s.listen(1)
+    # Set socket to be able to reconnect immediately and ignore timeouts
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Accept first connection and set connection to be class-wide
-print("searching for camera on port", TCP_PORT)
-conn, addr = s.accept()
-print("accepted connection")
+    # Bind socket to IP and port supplied from constructor
+    s.bind((TCP_IP, TCP_PORT))
 
-print("startLoop")
+    # Listen for exactly 1 host
+    s.listen(1)
+
+    # Accept first connection and set connection to be class-wide
+    print("Listening for camera on port", TCP_PORT)
+    conn, addr = s.accept()
+    print("Accepted connection")
+
+def loop():
+    print("Receiving stream")
+
+    global conn
+
+    while True:
+        try:
+            # Grab length data sent by client to correctly format image
+            length = recvall(conn, 16)
+
+            # Grab image data based on the length data previously recieved
+            if length is not None:
+                stringData = recvall(conn, int(length))
+            else:
+                print("No streaming data.")
+                cv2.destroyAllWindows()
+                break
+
+            # Decode JPG base64 string into a NumPy matrix of RGB data
+            data = numpy.frombuffer(stringData, dtype='uint8')
+
+            # Convert NumPy color matrix into OpenCV Matrix
+            decimg = cv2.imdecode(data, 1)
+
+            # Blow image back up to size
+            newX, newY = decimg.shape[1] * 2, decimg.shape[0] * 2
+            decimg = cv2.resize(decimg, (int(newX), int(newY)))
+
+            # Show image on a window named after its port.
+            cv2.imshow("Streaming video at %s port" % str(TCP_PORT), decimg)
+
+            # Use the OpenCV famous break statement to allow imshow to function properly.
+            k = cv2.waitKey(5) & 0xFF
+            if k == '+':
+                break
+
+        # If something breaks, say what happened
+        except Exception as e:
+            print("Broke in show loop")
+            print(traceback.format_exc())
+            break
+    # Close and destroy OpenCV stuff on crash.
+
 while True:
     try:
-        # Grab length data sent by client to correctly format image
-        length = recvall(conn, 16)
+        # Attempt to connect to socket
+        attemptConnection()
+        # Strat the playback loop
+        loop()
+    except ConnectionRefusedError:
+        pass
+        # Start the connection again if lost data
 
-        # Grab image data based on the length data previously recieved
-        if length is not None:
-            stringData = recvall(conn, int(length))
-        else:
-            print("data length 0")
-            break
-
-        # Decode JPG base64 string into a NumPy matrix of RGB data
-        data = numpy.frombuffer(stringData, dtype='uint8')
-
-        # Convert NumPy color matrix into OpenCV Matrix
-        decimg = cv2.imdecode(data, 1)
-
-        # Blow image back up to size
-        newX, newY = decimg.shape[1] * 2, decimg.shape[0] * 2
-        decimg = cv2.resize(decimg, (int(newX), int(newY)))
-
-        # Show image on a window named after its port.
-        cv2.imshow(str(TCP_PORT), decimg)
-
-        # Use the OpenCV famous break statement to allow imshow to function properly.
-        k = cv2.waitKey(5) & 0xFF
-        if k == '+':
-            break
-
-    # If something breaks, say what happened
-    except Exception as e:
-        print("Broke in show loop")
-        print(traceback.format_exc())
-        break
-# Close and destroy OpenCV stuff on crash.
